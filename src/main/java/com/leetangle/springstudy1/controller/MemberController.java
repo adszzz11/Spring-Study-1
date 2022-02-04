c192321a7443273e07e6161c7eb689cb38877e30
sm <adszzz11@naver.com>
2022-02-04 23:26:08 +0900

    step 11 / create MemberController

---
+++ src/main/java/com/leetangle/springstudy1/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.leetangle.springstudy1.controller;
+
+import com.leetangle.springstudy1.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
