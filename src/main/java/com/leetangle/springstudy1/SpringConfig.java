f030094f425c2b7fe9a89d5e8da97c0ad427f35d
sm <adszzz11@naver.com>
2022-02-08 22:29:28 +0900

    step 17 / modified SpringConfig - using jpa with EntityManager

--- src/main/java/com/leetangle/springstudy1/SpringConfig.java
+++ src/main/java/com/leetangle/springstudy1/SpringConfig.java
@@ -1,24 +1,23 @@
 package com.leetangle.springstudy1;
 
-import com.leetangle.springstudy1.repository.JdbcMemberRepository;
-import com.leetangle.springstudy1.repository.JdbcTemplateMemberRepository;
-import com.leetangle.springstudy1.repository.MemberRepository;
-import com.leetangle.springstudy1.repository.MemoryMemberRepository;
+import com.leetangle.springstudy1.repository.*;
 import com.leetangle.springstudy1.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    DataSource dataSource;
+    private final EntityManager em;
+
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -28,7 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
