eac8057e4552072ded048c10b6ca6e66fc4b1e73
sm <adszzz11@naver.com>
2022-02-09 21:24:44 +0900

    step 17 / modified SpringConfig -  using SpringDataJpaMemberRepository

--- src/main/java/com/leetangle/springstudy1/SpringConfig.java
+++ src/main/java/com/leetangle/springstudy1/SpringConfig.java
@@ -2,32 +2,26 @@ package com.leetangle.springstudy1;
 
 import com.leetangle.springstudy1.repository.*;
 import com.leetangle.springstudy1.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JpaMemberRepository(em);
+//    }
 
 }
