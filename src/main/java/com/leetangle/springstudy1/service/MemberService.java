d017c45fda9e9da4ce92cae6d8bf21e32b23693d
sm <adszzz11@naver.com>
2022-02-09 21:35:18 +0900

    step 18 / modified MemberService -  without AOP

--- src/main/java/com/leetangle/springstudy1/service/MemberService.java
+++ src/main/java/com/leetangle/springstudy1/service/MemberService.java
@@ -25,11 +25,19 @@ public class MemberService {
      * @return
      */
     public Long join(Member member) {
-        //중복이름 회원 회원가입 불가
-        validateDuplicateMember(member); //중복회원 검증
+        long start = System.currentTimeMillis();
+        try {
+            //중복이름 회원 회원가입 불가
+            validateDuplicateMember(member); //중복회원 검증
 
-        memberRepository.save(member);
-        return member.getId();
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish-start;
+            System.out.println("join = "+timeMs + "ms");
+        }
+
     }
 
     /**
@@ -37,7 +45,14 @@ public class MemberService {
      * @return
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish-start;
+            System.out.println("findMembers = "+timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
